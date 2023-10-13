10feb7c58747ad96a87d43d31643ba6bc45a9dd1
Sri Manikandan R <srimanikandandev@gmail.com>
2023-10-13 22:17:13 +0530

    Building the frontend-streamlit

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import openai
 import os
 from dotenv import load_dotenv
+import streamlit as st;
 load_dotenv()
-
-openai.api_key = os.getenv("OPENAI_KEY")
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Who won the world series in 2020?"}
-    ]
-)
-print(response["choices"][0]["message"]["content"])
+st.header("GPT-3 Chatbot")
+st.write("This is a simple chatbot that uses GPT-3 to answer questions. It is trained on a dataset of questions and answers from the internet. It is not perfect, but it is a good demonstration of the capabilities of GPT-3.")
+with st.form("my_form"):
+    title = st.text_input("Enter your question here:")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        openai.api_key = os.getenv("OPENAI_KEY")
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": title}
+            ]
+        )
+        st.write("The answer to your question is:")
+        st.write(response["choices"][0]["message"]["content"])
